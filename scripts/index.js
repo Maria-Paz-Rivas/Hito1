6df9c8dbd5b59d638c8afb9787857fda1b79a8d2
unknown <pazroscianorivas@gmail.com>
2024-07-25 15:59:18 -0300

    first push

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+
+class Activity {
+
+   constructor (id, title, description, imgUrl) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.imgUrl = imgUrl;
+   }
+}
+
+
+class Repository {
+   constructor () {
+        this.activities = [];
+        this.contador = 1;
+    }
+
+
+getAllActivities () {
+        return this.activities
+    }
+
+createActivity (title, description, imgUrl){
+    const activity = new Activity (this.contador, title, description, imgUrl);
+    this.activities.push(actividad)
+    this.contador++
+
+    return {
+        message: "Actividad creada con éxito",
+        actividad
+    }
+}
+
+deleteActivity (id) {
+    this.activities = this.activities.filter ((ele) => ele.id !== id)
+
+}
+
+}
